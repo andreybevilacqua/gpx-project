d35cf61694f1c0c017acde90171b172656952da3
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-16 22:13:51 +0200

    Adds basic interaction with the XML

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -1,1 +1,19 @@
-def file = new File()
+def file = new File('../data/fells_loop.gpx')
+
+def slurper = new XmlSlurper()
+def gpx = slurper.parse(file)
+
+printBasicInfos(gpx)
+
+gpx.rte.rtept.each {
+	println it.@lat
+	println it.@lon
+	println it.time
+}
+
+def printBasicInfos(gpx) {
+	println gpx.name
+	println gpx.desc
+	println gpx.@version
+	println gpx.@creator
+}
