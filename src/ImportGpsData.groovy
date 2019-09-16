c431fe4c338b216f5bd8b7ad10638ce0aa06b117
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-16 22:25:25 +0200

    Reads and generate another XML file from the previous one

--- src/ImportGpsData.groovy
+++ src/ImportGpsData.groovy
@@ -5,11 +5,21 @@ def gpx = slurper.parse(file)
 
 printBasicInfos(gpx)
 
-gpx.rte.rtept.each {
-	println it.@lat
-	println it.@lon
-	println it.time
+def markupBuilder = new groovy.xml.StreamingMarkupBuilder()
+def xml = markupBuilder.bind {
+	route {
+		mkp.comment(gpx.name)
+		gpx.rte.rtept.each { point ->
+			routepoint(timestamp: point.time.toString()) { // timestamp is the attribute from routepoint in xml
+				latitude(point.@lat)
+				longitude(point.@lon)
+			}
+		}
+	}
 }
+
+def outFile = new File('../data/fells_loop_truncated.xml')
+outFile.write(xml.toString())
 
 def printBasicInfos(gpx) {
 	println gpx.name
